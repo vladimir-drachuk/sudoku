6b43bf2adbd031d0d90bd791e2cdec56aa879cf9
vladimir-drachuk <vvdra4@gmail.com>
2019-10-19 23:48:35 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-///module.exports = function solveSudoku(matrix) {
-  
-//}
+module.exports = function solveSudoku(matrix) {
 
 
-function sudoku(matrix) {
+
+
+
+
+
+//function solveSudoku(matrix) {
+
   let reviseArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let reviseArray1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   let numb = 0;
+  let numb1 = 0;
+  let value = 0;
+  let valueI = 0;
+  let valueJ = 0;
   for (let i = 0; i < matrix.length; i++){
     for (let j = 0; j < matrix[i].length; j++){
       if (!matrix[i][j]) {
@@ -38,107 +47,49 @@ function sudoku(matrix) {
             }
           }
         }
-/////////////////////////////////////////////////////
-
-       if (reviseArray.length != 1 ) {
-
-        let array1 = [[0,0,0,0,0,0,0,0,0],
-                      [0,0,0,0,0,0,0,0,0],
-                      [0,0,0,0,0,0,0,0,0],
-                      [0,0,0,0,0,0,0,0,0],
-                      [0,0,0,0,0,0,0,0,0],
-                      [0,0,0,0,0,0,0,0,0],
-                      [0,0,0,0,0,0,0,0,0],
-                      [0,0,0,0,0,0,0,0,0],
-                      [0,0,0,0,0,0,0,0,0]];
-        let count = 0;
-        for (let p = 0; p < reviseArray.length; p++){
-
-          for (let aa = 0; aa < matrix.length; aa++){
-            for (let bb = 0; bb < matrix[aa].length; bb++){
-              array1[aa][bb] = matrix[aa][bb];
-            }
-          }
-
-          for (let l = posX; l < posX + 3; l++) {
-            for (let m = posY; m < posY + 3; m++) {
-              if (i === l && j === m) continue;
-              if (array1[l][m] === 0) {
-                for (let q = 0; q < array1.length; q++) {
-                  if (array1[l][q] === reviseArray[p]) {
-                    array1[l][m] = -1;
-                    count++
-                  } 
-                }
-                for (let q = 0; q < array1.length; q++) {
-                  if (array1[q][m] === reviseArray[p] && array1[l][m] !== -1) {
-                    array1[l][m] = -1;
-                    count++;
-                  } 
-                }
-              } else {
-                count++
-              }
-              
-            }
-          }
-          if (count === 8) {
-            matrix[i][j] = reviseArray[p];
-            i=0;
-            j=0;
-          }
-          count= 0;
-          array1 = [[0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0]];
-        }
-
-      }
-        /*for (let n = 0; n < reviseArray.length; n++) {
-          if (i-posX === 0) {
-            for (let p = posX; p < posX + 3; p++) {
-              if(array1[i+1][p] == 0) {
-                for (let s = 0; s < posX; s++) {
-                  if (array1[i+1][s] === array1[n]) {
-                    array1[i+1][p] = -1;
-                  }
-                }
-              }
-            }
-          }
-        }*/
-
 
         if (reviseArray.length === 1) {
           matrix[i][j] = reviseArray[0];
           i = 0;
-          j = 0;
+          j = -1;
+          value = 0;
         }
+        if (reviseArray.length !== 1)
+          if (value === 0) {
+            value = reviseArray[0];
+            valueI = i;
+            valueJ = j;
+          }
         reviseArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
       }
     }
   } 
+  if (value) {
+    matrix[valueI][valueJ] = value;
+    solveSudoku(matrix);
+  }
+  if (typeof(value === 'undefined')) {
+    for (let a = 0; a < 9; a++) {
+      numb1 = reviseArray1.indexOf(matrix[a][valueJ]);
+            if (numb1 > -1) {
+              reviseArray1.splice(numb1, 1);
+            }
+          }
+    for (let b = 0; b < 9; b++) {
+      if (matrix[valueI][b] === reviseArray1[0]) {
+        matrix[valueI][b] = reviseArray1[0];
+        console.log('matrix[valueI][b]', matrix[valueI][b]);
+        matrix[valueI][valueJ] = 0;
+      }
+    }
+
+    }
+  console.log(valueI);
+  console.log(valueJ);
+  console.log(value);
+  console.log('matrix', matrix);
   return matrix;
 }
 
 
-const initial = [
-  [6, 5, 0, 7, 3, 0, 0, 8, 0],
-    [0, 0, 0, 4, 8, 0, 5, 3, 0],
-    [8, 4, 0, 9, 2, 5, 0, 0, 0],
-    [0, 9, 0, 8, 0, 0, 0, 0, 0],
-    [5, 3, 0, 2, 0, 9, 6, 0, 0],
-    [0, 0, 6, 0, 0, 0, 8, 0, 0],
-    [0, 0, 9, 0, 0, 0, 0, 0, 6],
-    [0, 0, 7, 0, 0, 0, 0, 5, 0],
-    [1, 6, 5, 3, 9, 0, 4, 7, 0]
-  ]
-  
-console.log(initial)
-console.log(sudoku(initial));
+ 
